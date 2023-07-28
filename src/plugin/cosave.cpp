6be033f7a086bc4609b75d7a35756e41d30d00c4
C J Silverio <ceejceej@gmail.com>
2023-07-28 16:56:52 -0700

    when you think in u8 and they think in u32

--- src/plugin/cosave.cpp
+++ src/plugin/cosave.cpp
@@ -32,18 +32,12 @@ namespace cosave
 		auto pad                  = (buffer.size() % 16);
 		if (pad == 0) pad = 16;
 		logger::debug("cycles serialized into a Vec<u8> of size={}; pad={};"sv, buffer.size(), pad);
-		cosave->WriteRecordData(buffer.size() + pad);
-
-		for (uint8_t byte : buffer) { cosave->WriteRecordData(byte); }
+		cosave->WriteRecordData(buffer.size());
+		cosave->WriteRecordData(pad);
+		cosave->WriteRecordData(&buffer.data, buffer.size());
+		// for (uint8_t byte : buffer) { cosave->WriteRecordData(byte); }
 		for (int i = 0; i < pad; i++) { cosave->WriteRecordData(0); }
 	}
-
-	/*
-	void revertHandler(SKSE::SerializationInterface* cosave)
-	{
-		// TODO reset
-	}
-*/
 
 	void gameLoadedHandler(SKSE::SerializationInterface* cosave)
 	{
@@ -55,10 +49,12 @@ namespace cosave
 		{
 			if (type == CYCLE_RECORD)
 			{
-				int bufferSize;
+				size_t bufferSize;
+				size_t pad;
 				rust::Vec<uint8_t> buffer;
 				cosave->ReadRecordData(bufferSize);
-				logger::debug("found our cosave data; need to read a buffer of size={}"sv, bufferSize);
+				cosave->ReadRecordData(pad);
+				logger::debug("found our cosave data; need to read a buffer of size={}; pad={}"sv, bufferSize, pad);
 
 				for (; bufferSize > 0; --bufferSize)
 				{
@@ -73,4 +69,10 @@ namespace cosave
 		}
 	}
 
+	/*
+	void revertHandler(SKSE::SerializationInterface* cosave)
+	{
+		// TODO reset if anything to do ?
+	}
+	*/
 }
