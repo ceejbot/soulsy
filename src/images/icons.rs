fb827e30951b405f169fe4e1c74e4fbf1e81f02f
C J Silverio <ceejceej@gmail.com>
2023-11-22 16:58:34 -0800

    Icon names are now generated from enum variant name
    This means the naming convention is now enforced. Renamed the
    icons that didn't fit the convention. Separated the THICC icon
    checking test from the Soulsy icon checking test, and marked it
    as ignored so we don't fail test runs. (There are a lot of
    icons missing from that pack.)
    
    Removed the annoying Windows execute bit from some icons.

--- src/images/icons.rs
+++ src/images/icons.rs
@@ -1,6 +1,7 @@
 use strum::{Display, EnumString, EnumVariantNames};
 
 #[derive(Debug, Clone, Default, Hash, PartialEq, Eq, EnumString, EnumVariantNames, Display)]
+#[strum(serialize_all = "snake_case")]
 pub enum Icon {
     Alteration,
     AmmoArrow,
@@ -181,181 +182,7 @@ pub enum Icon {
 
 impl Icon {
     pub fn icon_file(&self) -> String {
-        match &self {
-            // Icon::SpellArcane => "spell_arcane.svg".to_string(),
-            // Icon::SpellBlast => "spell_blast.svg".to_string(),
-            // Icon::SpellBolt => "spell_bolt.svg".to_string(),
-            // Icon::SpellChainLightning => "spell_chain_lightning.svg".to_string(),
-            // Icon::SpellFireDual => "spell_fire_dual.svg".to_string(),
-            // Icon::SpellFireWall => "spell_fire_wall.svg".to_string(),
-            // Icon::SpellFireball => "spell_fireball.svg".to_string(),
-            // Icon::SpellFrostWall => "spell_frost_wall.svg".to_string(),
-            // Icon::SpellIceShard => "spell_ice_shard.svg".to_string(),
-            // Icon::SpellShockStrong => "spell_shock_strong.svg".to_string(),
-            // Icon::SpellSmoke => "spell_smoke.svg".to_string(),
-            // Icon::SpellWave => "spell_wave.svg".to_string(),
-            Icon::Alteration => "alteration.svg".to_string(),
-            Icon::AmmoArrow => "ammo_arrow.svg".to_string(),
-            Icon::AmmoBullet => "ammo_bullet.svg".to_string(),
-            Icon::ArmorAmulet => "armor_amulet.svg".to_string(),
-            Icon::ArmorBackpack => "armor_backpack.svg".to_string(),
-            Icon::ArmorBelt => "armor_belt.svg".to_string(),
-            Icon::ArmorBracelet => "armor_bracelet.svg".to_string(),
-            Icon::ArmorCirclet => "armor_circlet.svg".to_string(),
-            Icon::ArmorCloak => "armor_cloak.svg".to_string(),
-            Icon::ArmorClothing => "armor_clothing.svg".to_string(),
-            Icon::ArmorClothingFeet => "armor_clothing_feet.svg".to_string(),
-            Icon::ArmorClothingHands => "armor_clothing_hands.svg".to_string(),
-            Icon::ArmorClothingHead => "armor_clothing_head.svg".to_string(),
-            Icon::ArmorEarring => "armor_earring.svg".to_string(),
-            Icon::ArmorHeavy => "armor_heavy.svg".to_string(),
-            Icon::ArmorHeavyFeet => "armor_heavy_feet.svg".to_string(),
-            Icon::ArmorHeavyHands => "armor_heavy_hands.svg".to_string(),
-            Icon::ArmorHeavyHead => "armor_heavy_head.svg".to_string(),
-            Icon::ArmorLight => "armor_light.svg".to_string(),
-            Icon::ArmorLightFeet => "armor_light_feet.svg".to_string(),
-            Icon::ArmorLightHands => "armor_light_hands.svg".to_string(),
-            Icon::ArmorLightHead => "armor_light_head.svg".to_string(),
-            Icon::ArmorMask => "armor_mask.svg".to_string(),
-            Icon::ArmorQuiver => "armor_quiver.svg".to_string(),
-            Icon::ArmorRing => "armor_ring.svg".to_string(),
-            Icon::ArmorRobes => "armor_robes.svg".to_string(),
-            Icon::ArmorShieldHeavy => "armor_shield_heavy.svg".to_string(),
-            Icon::ArmorShieldLight => "armor_shield_light.svg".to_string(),
-            Icon::Conjuration => "conjuration.svg".to_string(),
-            Icon::Destruction => "destruction.svg".to_string(),
-            Icon::DrinkMead => "drink_mead.svg".to_string(),
-            Icon::DrinkTea => "drink_tea.svg".to_string(),
-            Icon::DrinkWater => "drink_water.svg".to_string(),
-            Icon::DrinkWine => "drink_wine.svg".to_string(),
-            Icon::Food => "food.svg".to_string(),
-            Icon::FoodBread => "food_bread.svg".to_string(),
-            Icon::FoodCarrot => "food_carrot.svg".to_string(),
-            Icon::FoodCheese => "food_cheese.svg".to_string(),
-            Icon::FoodFish => "food_fish.svg".to_string(),
-            Icon::FoodMeat => "food_meat.svg".to_string(),
-            Icon::FoodPie => "food_pie.svg".to_string(),
-            Icon::FoodStew => "food_stew.svg".to_string(),
-            Icon::HandToHand => "hand_to_hand.svg".to_string(),
-            Icon::IconDefault => "icon_default.svg".to_string(),
-            Icon::Illusion => "illusion.svg".to_string(),
-            Icon::MiscLantern => "misc_lantern.svg".to_string(),
-            Icon::MiscCampfire => "misc_campfire.svg".to_string(),
-            Icon::MiscLute => "misc_lute.svg".to_string(),
-            Icon::MiscTent => "misc_tent.svg".to_string(),
-            Icon::PotionDefault => "potion_default.svg".to_string(),
-            Icon::PotionHealth => "potion_health.svg".to_string(),
-            Icon::PotionMagicka => "potion_magicka.svg".to_string(),
-            Icon::PotionPoison => "potion_poison.svg".to_string(),
-            Icon::PotionResist => "potion_resist.svg".to_string(),
-            Icon::PotionResistFire => "potion_resist_fire.svg".to_string(),
-            Icon::PotionResistFrost => "potion_resist_frost.svg".to_string(),
-            Icon::PotionResistShock => "potion_resist_shock.svg".to_string(),
-            Icon::PotionSkooma => "potion_skooma.svg".to_string(),
-            Icon::PotionStamina => "potion_stamina.svg".to_string(),
-            Icon::Power => "power.svg".to_string(),
-            Icon::PowerCraft => "power_craft.svg".to_string(),
-            Icon::PowerFillBottles => "power_fill_bottles.svg".to_string(),
-            Icon::PowerHorse => "power_horse.svg".to_string(),
-            Icon::PowerPeek => "power_horse.svg".to_string(),
-            Icon::PowerPray => "power_pray.svg".to_string(),
-            Icon::PowerWash => "power_wash.svg".to_string(),
-            Icon::PowerWerebear => "power_werebear.svg".to_string(),
-            Icon::PowerWerewolf => "power_werewolf.svg".to_string(),
-            Icon::Restoration => "restoration.svg".to_string(),
-            Icon::Scroll => "scroll.svg".to_string(),
-            Icon::Shout => "shout.svg".to_string(),
-            Icon::ShoutAnimalAllegiance => "shout_animal_allegiance.svg".to_string(),
-            Icon::ShoutBreathAttack => "shout_breath_attack.svg".to_string(),
-            Icon::ShoutCallDragon => "shout_call_dragon.svg".to_string(),
-            Icon::ShoutClearSkies => "shout_clear_skies.svg".to_string(),
-            Icon::ShoutCyclone => "shout_cyclone.svg".to_string(),
-            Icon::ShoutDismay => "shout_dismay.svg".to_string(),
-            Icon::ShoutElementalFury => "spell_elementalfury.svg".to_string(),
-            Icon::ShoutIceForm => "shout_ice_form.svg".to_string(),
-            Icon::ShoutMarkedForDeath => "shout_marked_death.svg".to_string(),
-            Icon::ShoutStormcall => "shout_stormcall.svg".to_string(),
-            Icon::ShoutUnrelentingForce => "shout_unrelentingforce.svg".to_string(),
-            Icon::Soulgem => "soulgem.svg".to_string(),
-            Icon::SpellArclight => "spell_arclight.svg".to_string(),
-            Icon::SpellBleed => "spell_bleed.svg".to_string(),
-            Icon::SpellCircle => "spell_circle.svg".to_string(),
-            Icon::SpellConstellation => "spell_constellation.svg".to_string(),
-            Icon::SpellControl => "spell_control.svg".to_string(),
-            Icon::SpellCure => "spell_cure.svg".to_string(),
-            Icon::SpellDesecration => "spell_desecration.svg".to_string(),
-            Icon::SpellDetect => "spell_detect.svg".to_string(),
-            // Icon::SpellDrain => "spell_drain.svg".to_string(),
-            Icon::SpellEagleEye => "spell_eagleeye.svg".to_string(),
-            Icon::SpellEarth => "spell_earth.svg".to_string(),
-            Icon::SpellEvade => "spell_evade.svg".to_string(),
-            Icon::SpellFear => "spell_fear.svg".to_string(),
-            Icon::SpellFeather => "spell_feather.svg".to_string(),
-            Icon::SpellFire => "spell_fire.svg".to_string(),
-            Icon::SpellFrost => "spell_frost.svg".to_string(),
-            Icon::SpellHeal => "spell_heal.svg".to_string(),
-            Icon::SpellHoly => "spell_holy.svg".to_string(),
-            Icon::SpellLeaf => "spell_leaf.svg".to_string(),
-            Icon::SpellLeaves => "spell_leaves.svg".to_string(),
-            Icon::SpellLight => "spell_light.svg".to_string(),
-            // Icon::SpellLightning => "spell_lightning.svg".to_string(),
-            // Icon::SpellLightningBall => "spell_lightning_ball.svg".to_string(),
-            Icon::SpellLightningBlast => "spell_lightning_blast.svg".to_string(),
-            // Icon::SpellMeteor => "spell_meteor.svg".to_string(),
-            Icon::SpellMoon => "spell_moon.svg".to_string(),
-            Icon::SpellParalyze => "spell_paralyze.svg".to_string(),
-            Icon::SpellPoison => "spell_poison.svg".to_string(),
-            Icon::SpellReanimate => "spell_reanimate.svg".to_string(),
-            Icon::SpellReflect => "spell_reflect.svg".to_string(),
-            Icon::SpellRoot => "spell_root.svg".to_string(),
-            Icon::SpellRune => "spell_rune.svg".to_string(),
-            Icon::SpellShadow => "spell_shadow.svg".to_string(),
-            Icon::SpellSharpen => "spell_sharpen.svg".to_string(),
-            Icon::SpellShock => "spell_shock.svg".to_string(),
-            Icon::SpellSilence => "spell_silence.svg".to_string(),
-            Icon::SpellSlow => "spell_slow.svg".to_string(),
-            Icon::SpellSoultrap => "spell_soultrap.svg".to_string(),
-            Icon::SpellSprint => "spell_sprint.svg".to_string(),
-            Icon::SpellStamina => "spell_stamina.svg".to_string(),
-            Icon::SpellStars => "spell_stars.svg".to_string(),
-            Icon::SpellSummon => "spell_summon.svg".to_string(),
-            Icon::SpellSun => "spell_sun.svg".to_string(),
-            Icon::SpellTeleport => "spell_teleport.svg".to_string(),
-            Icon::SpellTime => "spell_time.svg".to_string(),
-            Icon::SpellVampire => "spell_vampire.svg".to_string(),
-            Icon::SpellWard => "spell_ward.svg".to_string(),
-            Icon::SpellWater => "spell_water.svg".to_string(),
-            Icon::SpellWind => "spell_wind.svg".to_string(),
-            Icon::SpellWisp => "spell_wisp.svg".to_string(),
-            Icon::ToolFishingRod => "tool_fishingrod.svg".to_string(),
-            Icon::ToolPickaxe => "tool_pickaxe.svg".to_string(),
-            Icon::ToolShovel => "tool_shovel.svg".to_string(),
-            Icon::ToolSickle => "tool_sickle.svg".to_string(),
-            Icon::MiscTorch => "misc_torch.svg".to_string(),
-            Icon::WeaponAxeOneHanded => "weapon_axe_one_handed.svg".to_string(),
-            Icon::WeaponAxeTwoHanded => "weapon_axe_two_handed.svg".to_string(),
-            Icon::WeaponBow => "weapon_bow.svg".to_string(),
-            Icon::WeaponClaw => "weapon_claw.svg".to_string(),
-            Icon::WeaponCrossbow => "weapon_crossbow.svg".to_string(),
-            Icon::WeaponDagger => "weapon_dagger.svg".to_string(),
-            Icon::WeaponFlail => "weapon_flail.svg".to_string(),
-            Icon::WeaponGrenade => "weapon_grenade.svg".to_string(),
-            Icon::WeaponGun => "weapon_gun.svg".to_string(),
-            Icon::WeaponHalberd => "weapon_halberd.svg".to_string(),
-            Icon::WeaponHammer => "weapon_hammer.svg".to_string(),
-            Icon::WeaponKatana => "weapon_katana.svg".to_string(),
-            Icon::WeaponLance => "weapon_lance.svg".to_string(),
-            Icon::WeaponMace => "weapon_mace.svg".to_string(),
-            Icon::WeaponPike => "weapon_pike.svg".to_string(),
-            Icon::WeaponQuarterstaff => "weapon_quarterstaff.svg".to_string(),
-            Icon::WeaponRapier => "weapon_rapier.svg".to_string(),
-            Icon::WeaponScythe => "weapon_scythe.svg".to_string(),
-            Icon::WeaponStaff => "weapon_staff.svg".to_string(),
-            Icon::WeaponSwordOneHanded => "weapon_sword_one_handed.svg".to_string(),
-            Icon::WeaponSwordTwoHanded => "weapon_sword_two_handed.svg".to_string(),
-            Icon::WeaponWhip => "weapon_whip.svg".to_string(),
-            Icon::WeaponWoodAxe => "weapon_woodaxe.svg".to_string(),
-        }
+        format!("{}.svg", self.to_string())
     }
 
     /// Fall back from any icon to one in the core set guaranteed to come with the base HUD.
@@ -562,9 +389,8 @@ impl Icon {
 
 #[cfg(test)]
 pub fn random_icon() -> Icon {
-    use std::str::FromStr;
-
     use rand::prelude::*;
+    use std::str::FromStr;
     use strum::VariantNames;
 
     if let Some(variant) = Icon::VARIANTS.choose(&mut rand::thread_rng()) {
@@ -576,35 +402,58 @@ pub fn random_icon() -> Icon {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use std::path::PathBuf;
     use std::str::FromStr;
-
-    use once_cell::sync::Lazy;
     use strum::VariantNames;
 
-    use super::*;
-
-    static ICON_PATHS: Lazy<Vec<&str>> = Lazy::new(|| {
-        vec![
+    #[test]
+    fn soulsy_pack_complete() {
+        let icon_paths = vec![
             "data/SKSE/plugins/resources/icons/",
             "layouts/icon-pack-soulsy",
-            "layouts/icon-pack-thicc",
-        ]
-    });
+        ];
+        let missing: Vec<&&str> = Icon::VARIANTS
+            .iter()
+            .filter(|variant| {
+                let icon =
+                    Icon::from_str(variant).expect("icon names should darn well turn into icons");
+                let found = icon_paths.iter().any(|prefix| {
+                    let fpath: PathBuf = [prefix, icon.icon_file().as_str()].iter().collect();
+                    fpath.exists()
+                });
+                if !found {
+                    eprintln!("svg for {icon:?} missing: {}", icon.icon_file());
+                }
+                !found
+            })
+            .collect();
+        assert!(missing.len() == 0);
+    }
 
     #[test]
-    fn icons_have_files() {
-        Icon::VARIANTS.iter().for_each(|variant| {
-            let icon =
-                Icon::from_str(variant).expect("icon names should darn well turn into icons");
-            let found = ICON_PATHS.iter().any(|prefix| {
-                let fpath: PathBuf = [prefix, icon.icon_file().as_str()].iter().collect();
-                fpath.exists()
-            });
-            if !found {
-                eprintln!("svg for {icon:?} missing: {}", icon.icon_file());
-            }
-            assert!(found);
-        });
+    #[ignore]
+    fn thicc_pack_complete() {
+        let icon_paths = vec![
+            "data/SKSE/plugins/resources/icons/",
+            "layouts/icon-pack-thicc",
+        ];
+        let missing: Vec<String> = Icon::VARIANTS
+            .iter()
+            .filter_map(|variant| {
+                let icon =
+                    Icon::from_str(variant).expect("icon names should darn well turn into icons");
+                let found = icon_paths.iter().any(|prefix| {
+                    let fpath: PathBuf = [prefix, icon.icon_file().as_str()].iter().collect();
+                    fpath.exists()
+                });
+                if !found {
+                    Some(format!("MISSING {icon:?}: {}", icon.icon_file()))
+                } else {
+                    None
+                }
+            })
+            .collect();
+        assert!(missing.len() == 0, "{missing:#?}");
     }
 }
