33638bb90da70e1a73bac4eab057ed6ee15fd02f
C J Silverio <ceejceej@gmail.com>
2023-11-24 12:14:44 -0800

    Uh...

--- src/layouts/layout_v2.rs
+++ src/layouts/layout_v2.rs
@@ -260,8 +260,8 @@ mod tests {
 
     #[test]
     #[ignore]
-    // The github runner doesn't seem to have a data directory. I have NO IDEA why not yet.
     fn default_layout_valid() {
+        // The github runner doesn't seem to have a data directory. I have NO IDEA why not yet.
         let data = include_str!("../../data/SKSE/plugins/SoulsyHUD_layout.toml");
         let builtin: Layout = toml::from_str(data).expect("layout should be valid toml");
         match builtin {
